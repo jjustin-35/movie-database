e594315a6d2d2aecacc6da7fd5924b351db12da4
jjustin-35 <jjusttin11@gmail.com>
2025-07-20 02:04:14 +0800

    feat: add performance monitor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "@/context";
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights />
         <Provider>
           <Header />
           <Toast />
