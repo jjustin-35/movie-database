0dd2e8347382393f210083a9575173685501f493
jjustin-35 <jjusttin11@gmail.com>
2025-07-21 02:24:55 +0800

    update: orderlist type

--- helpers/orderList.ts
+++ helpers/orderList.ts
@@ -3,7 +3,7 @@ import { Movie, OrderType } from "@/constants/type";
 export const orderList = ({order, type, list}: {order: "asc" | "desc", type: OrderType, list: Movie[]}) => {
     const newList = [...list].sort((a, b) => {
       const [aValue, bValue] = (() => {
-        if (type === "release_date") {
+        if (type === OrderType.releaseDate) {
           return [new Date(a.release_date).getTime(), new Date(b.release_date).getTime()];
         }
         return [a[type], b[type]];
